66845c8be245c8a6225677892ba70f6acd995b4b
Henry Esteban Sosof Sisay <108730147+Respauneado@users.noreply.github.com>
2023-12-04 23:12:55 +0000

    segundo intento

--- member.js
+++ member.js
@@ -1,9 +1,11 @@
 function skillMember() {
-    var member = document.getElementById("member");
-    var memberValue = member.options[member.selectedIndex].value;
-    if (memberValue == "Yes") {
-        document.getElementById("skillMember").style.display = "block";
-    } else {
-        document.getElementById("skillMember").style.display = "none";
-    }
+  return {
+    restrict: 'E',
+    scope: {
+      member: '='
+    },
+    templateUrl: 'templates/skill-member.html'
+  };
 }
+
+   
