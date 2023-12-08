2fc314d924670abd8f780c6458c92912d3ab4467
Henry Esteban Sosof Sisay <108730147+Respauneado@users.noreply.github.com>
2023-12-08 17:26:28 +0000

    segundo commit con opciones de copiloto

--- member.js
+++ member.js
@@ -1,11 +1,12 @@
 function skillMember() {
-  return {
-    restrict: 'E',
-    scope: {
-      member: '='
-    },
-    templateUrl: 'templates/skill-member.html'
-  };
-}
+    var x = document.getElementById("skillMember");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      document.getElementById("skillMember").style.display = "block";
+      document.getElementById("skillMember").style.transition = "all 2s";
+    } else {
+      x.style.display = "none";
+    }
+  }
 
    
